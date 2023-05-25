a1ad35b945db21459f5a72beb715be66ddaf8dc4
Gregor Wolf <gregor.wolf@gmail.com>
2023-05-25 22:40:40 +0200

    add service consumption

--- srv/mashup-service.js
+++ srv/mashup-service.js
@@ -1,3 +1,13 @@
-module.exports = (say) => {
-  say.on("Hello", (req) => `Hello ${req.data.name}!`);
-};
+const cds = require("@sap/cds");
+const LOG = cds.log("dataload");
+
+module.exports = cds.service.impl(async (srv) => {
+  const bpService = await cds.connect.to("API_BUSINESS_PARTNER");
+
+  srv.on("READ", "BusinessPartners", (req) => {
+    LOG.info("READ BusinessPartners");
+    return bpService.run(req.query);
+  });
+
+  srv.on("Hello", (req) => `Hello ${req.data.name}!`);
+});
